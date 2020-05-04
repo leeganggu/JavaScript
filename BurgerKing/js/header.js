d3bf8667319c17ccf6d86773fd2d64c8c71bb35e
ggLee <spahrose@naver.com>
2020-05-04 14:57:38 +0900

    20200504

---
+++ BurgerKing/js/header.js
@@ -0,0 +1,6 @@
+let headerDiv = document.createElement("div");
+
+headerDiv.setAttribute("class", "head_container");
+
+headerDiv.innerHTML = "aa";
+//eaderDiv.innerHTML = "<a herf='#/home.html'><h1 class='WEB logo'>버거킹 Logo</h1></a>";
